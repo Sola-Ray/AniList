bfddd8dd8ec2cd7349cb13dc36f869d9d2e7e362
Baptiste <Baptiste.SOUSA@etu.uca.fr>
2021-06-20 16:31:39 +0200

    [UPDATE] Typo in fav anime page

--- anime-list/src/app/modules/home/profile/favorite-anime/favorite-anime.page.ts
+++ anime-list/src/app/modules/home/profile/favorite-anime/favorite-anime.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {DatabaseService} from '../../../../service/database.service';
 import {Router} from '@angular/router';
 
@@ -7,7 +7,7 @@ import {Router} from '@angular/router';
   templateUrl: './favorite-anime.page.html',
   styleUrls: ['./favorite-anime.page.scss'],
 })
-export class FavoriteAnimePage implements OnInit {
+export class FavoriteAnimePage implements OnInit, AfterViewInit {
   favorites: any = [];
   constructor(private database: DatabaseService,
               private router: Router,) { }
@@ -16,9 +16,9 @@ export class FavoriteAnimePage implements OnInit {
   }
   async ngAfterViewInit(): Promise<void> {
     await this.database.init();
-    await this.database.openStore("favoriteAnime");
+    await this.database.openStore('favoriteAnime');
 
-    let values = await this.database.getAllValues();
+    const values = await this.database.getAllValues();
     for(let i = 0; i < values.length; i++) {
       this.favorites.push(JSON.parse(values[i]));
     }
